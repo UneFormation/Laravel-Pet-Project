0790830ad7ee330624d125f1821c4a64bc5e8fc2
David Patiashvili <stratagem.david@gmail.com>
2022-10-09 19:12:41 +0200

    Admin : Display surveys list

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Survey;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(Request $request)
     {
-        return view('admin.index');
+        $surveys = Survey::query()
+            ->orderBy('id', 'desc')
+            ->paginate(15);
+
+        return view('admin.index', ['surveys' => $surveys]);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,32 @@
 @extends('admin.base')
 
 @section('content')
-    Hello
+    @if(!$surveys->total())
+        <div class="alert alert-primary">Not exist surveys.</div>
+    @else
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Firstname</th>
+                    <th scope="col">Lastname</th>
+                    <th scope="col">email</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($surveys as $survey)
+                <tr>
+                    <th scope="row">{{$survey->id}}</th>
+                    <td>{{$survey->firstname}}</td>
+                    <td>{{$survey->lastname}}</td>
+                    <td>{{$survey->email}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <ul class="pagination justify-content-center mb-4">
+            {{$surveys->links("pagination::bootstrap-4")}}
+        </ul>
+    @endif
 @endsection
